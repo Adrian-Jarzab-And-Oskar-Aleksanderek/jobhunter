6b3eb393024b6ddf0e3f8ce2566324b11f8bc00b
Oskar Aleksanderek <o.aleksanderek.615@studms.ug.edu.pl>
2024-10-12 17:08:39 +0200

    update gitignore

--- Backend/obj/Release/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Release/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20dbcc315a307823517dc572007bedc5b9048f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
