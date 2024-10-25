56c95274c73bcf209c49b62299b1b018433d1144
Kodiii <niewiemkurwa133@gmail.com>
2024-10-25 18:42:32 +0200

    changed footer

---
+++ Frontend/src/Pages/HomePage.tsx
@@ -0,0 +1,33 @@
+import 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import Footer from './Footer';
+
+const HomePage = () => {
+    return (
+        <>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand className="me-auto" href="/">JobHunter</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <LinkContainer to="/">
+                                <Nav.Link>Home</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/about">
+                                <Nav.Link>About</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/contact">
+                                <Nav.Link>Contact</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Footer />
+        </>
+    );
+}
+
+export default HomePage;
