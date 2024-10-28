e5e0e5481dea119649efb0c25cb73a78e29a6112
Kodiii <niewiemkurwa133@gmail.com>
2024-10-28 21:12:10 +0100

    Scraper

---
+++ Scraper/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Scraper;
+
+internal class Program 
+{
+    public static void Main(string[] args) {
+       Scraper.Scrap("https://www.pracuj.pl/praca/data-warehouse-analyst-warszawa,oferta,1003658385?sug=oferta_bottom_bd_1_tname_214_tgroup_A_promoted", "div[data-test=\"text-earningAmount\"]");
+    }
+}
---
+++ Scraper/ScrapWebsite.cs
@@ -0,0 +1,42 @@
+﻿namespace Scraper;
+
+public class ScrapWebsite {
+    private static string _url;
+    private static readonly Dictionary<string, string?> JobOfferValues;
+    private static Dictionary<string, string> _query;
+    static ScrapWebsite()
+    {
+        JobOfferValues = new Dictionary<string, string?>
+        {
+        { "companyName", null },
+        { "salaryRange", null },
+        { "salaryMax", null },
+        { "salaryMin", null },
+        { "locationsName", null },
+        { "remote", null },
+        { "expired", null },
+        { "technologyStack", null },
+        { "employmentType", null },
+        { "experienceType", null },
+        { "workType", null }
+        };
+    }
+
+    public ScrapWebsite(string url, Dictionary<string, string> query) {
+        _url = url;
+        _query = query; 
+    }
+
+    public static Dictionary<string, string?> ScrapData() {
+        foreach (string key in _query.Keys) {
+            if (JobOfferValues.ContainsKey(key)) {
+                JobOfferValues.Add(key, Scraper.Scrap(_url, _query[key]));
+            }
+        }
+        return JobOfferValues;
+    }
+
+    public static void AddNewPosition(string positionName) {
+        JobOfferValues.Add(positionName, null);
+    }
+}
