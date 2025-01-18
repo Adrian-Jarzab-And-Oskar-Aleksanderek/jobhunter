c790c2b9eee84e125e2b057fd2149586b7312f10
Kordiii <a.jarzab03@gmail.com>
2025-01-18 23:25:42 +0100

    add new relation

--- Backend/Controllers/ReviewControler.cs
+++ Backend/Controllers/ReviewControler.cs
@@ -6,43 +6,43 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
+    // [ApiController]
+    // [Route("api/[controller]")]
     public class ReviewControler : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
-        private readonly UserManager<IdentityUser> _userManager;
-
-        public ReviewControler(ApplicationDbContext context, UserManager<IdentityUser> userManager)
-        {
-            _context = context;
-            _userManager = userManager;
-        }
-
-        [HttpPost("create")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Comment, Rating")] Review review, int jobOfferId)
-        {
-            // var userId = _userManager.GetUserId(User);
-            // if (string.IsNullOrEmpty(userId))
-            // {
-            //     return RedirectToAction("Login", "Account");
-            // }
-
-            var jobOffer = await _context.JobOffers.FindAsync(jobOfferId);
-            if (jobOffer == null)
-            {
-                return NotFound("Oferta pracy nie została znaleziona.");
-            }
-
-            review.UserId = "741d24fc-739f-4b1d-add1-c4ab006ee0c5";
-            review.JobOfferId = jobOffer.Id;
-            review.CreatedAt = DateTime.Now;
-
-            _context.Add(review);
-            await _context.SaveChangesAsync();
-
-            return Ok(new { message = "Recenzja została dodana", review });
-        }
+        // private readonly ApplicationDbContext _context;
+        // private readonly UserManager<IdentityUser> _userManager;
+        //
+        // public ReviewControler(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        // {
+        //     _context = context;
+        //     _userManager = userManager;
+        // }
+        //
+        // [HttpPost("create")]
+        // [ValidateAntiForgeryToken]
+        // public async Task<IActionResult> Create([Bind("Comment, Rating")] Review review, int jobOfferId)
+        // {
+        //     // var userId = _userManager.GetUserId(User);
+        //     // if (string.IsNullOrEmpty(userId))
+        //     // {
+        //     //     return RedirectToAction("Login", "Account");
+        //     // }
+        //
+        //     var jobOffer = await _context.JobOffers.FindAsync(jobOfferId);
+        //     if (jobOffer == null)
+        //     {
+        //         return NotFound("Oferta pracy nie została znaleziona.");
+        //     }
+        //
+        //     review.UserId = "741d24fc-739f-4b1d-add1-c4ab006ee0c5";
+        //     review.JobOfferId = jobOffer.Id;
+        //     review.CreatedAt = DateTime.Now;
+        //
+        //     _context.Add(review);
+        //     await _context.SaveChangesAsync();
+        //
+        //     return Ok(new { message = "Recenzja została dodana", review });
+        // }
     }
 }
---
+++ Backend/Migrations/20250118222344_AddReviewsTable.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReviewsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_AspNetUsers_UserId",
+                table: "Review");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_JobOffers_JobOfferId",
+                table: "Review");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Review",
+                table: "Review");
+
+            migrationBuilder.RenameTable(
+                name: "Review",
+                newName: "Reviews");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Review_UserId",
+                table: "Reviews",
+                newName: "IX_Reviews_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Review_JobOfferId",
+                table: "Reviews",
+                newName: "IX_Reviews_JobOfferId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Reviews",
+                table: "Reviews",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_UserId",
+                table: "Reviews",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_JobOffers_JobOfferId",
+                table: "Reviews",
+                column: "JobOfferId",
+                principalTable: "JobOffers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_UserId",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_JobOffers_JobOfferId",
+                table: "Reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Reviews",
+                table: "Reviews");
+
+            migrationBuilder.RenameTable(
+                name: "Reviews",
+                newName: "Review");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reviews_UserId",
+                table: "Review",
+                newName: "IX_Review_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reviews_JobOfferId",
+                table: "Review",
+                newName: "IX_Review_JobOfferId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Review",
+                table: "Review",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_AspNetUsers_UserId",
+                table: "Review",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_JobOffers_JobOfferId",
+                table: "Review",
+                column: "JobOfferId",
+                principalTable: "JobOffers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
